c6cd9e2734bbbe4e7c14d3d0f2432d7eab6254c8
MarcosGOFavaretto <marcosfavaretto.dev@gmail.com>
2023-03-12 15:02:56 -0300

    :package:BUILD: Updates the bash file to build the new plugin

--- runBuildAndTest.sh
+++ runBuildAndTest.sh
@@ -5,12 +5,20 @@ mvn clean install;
 unzip -o target/cotuba-cli-0.0.1-SNAPSHOT-distribution.zip -d ../;
 cd ..;
 
-# CONSTRUINDO O PLUGIN
+# CONSTRUINDO OS PLUGINS
 
+# Tema
 cd ./tema-paradizo;
 mvn clean package;
 cp target/tema-paradizo-0.0.1-SNAPSHOT.jar ../libs/;
 cd ..;
+
+# Estatisticas
+cd ./estatisticas-ebook;
+mvn clean package;
+cp target/estatisticas-ebook-0.0.1-SNAPSHOT.jar ../libs/;
+cd ..;
+
 cp ~/.m2/repository/org/jsoup/jsoup/1.11.2/jsoup-1.11.2.jar ./libs/;
 
 # TESTANDO
